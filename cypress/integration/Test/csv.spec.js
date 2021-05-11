fbdcc3ba635e08115fff3db47e4d46e0600a0630
unknown <FINTRAK QA@FintrakQA.fintraksoftware.com>
2021-05-11 22:45:29 +0100

    second commit

--- cypress/integration/Test/csv.spec.js
+++ cypress/integration/Test/csv.spec.js
@@ -2,7 +2,7 @@ describe('Billable Hours', () => {
 
     it('Ensure User can access Url', () => {
         //Application URL
-        cy.visit('https://csvdemomockappp.bundlewallet.com/')
+        cy.visit('https://csvdemomockappp.bundlewallet.com/');
     }) 
 
     it('Ensure user can Upload', () => {
@@ -12,24 +12,24 @@ describe('Billable Hours', () => {
         cy.get('#statement-file').attachFile(fixtureFile);
 
         //Parse New File button
-        cy.get('.btn').click()
+        cy.get('.btn').click();
     })
 
     it('Ensure User can view company details', () => {
         //One of the companies displyed is selected here to view for more billable details
-        cy.get(':nth-child(1) > :nth-child(2) > .ng-binding').click()
-        cy.wait(4000)
+        cy.get(':nth-child(1) > :nth-child(2) > .ng-binding').click();
+        cy.wait(4000);
 
         //To go back to results in order to select another company for more billable details
-        cy.contains('Back to Result').click()
-        cy.wait(4000)
+        cy.contains('Back to Result').click();
+        cy.wait(4000);
 
         //One of the companies displyed is selected here to view for more billable details
-        cy.get('tbody > :nth-child(2) > :nth-child(2) > .ng-binding').click()
-        cy.wait(4000)
+        cy.get('tbody > :nth-child(2) > :nth-child(2) > .ng-binding').click();
+        cy.wait(4000);
 
         //This is to allow user to parse new file
-        cy.get('[ng-href="#!/"]').click()
+        cy.get('[ng-href="#!/"]').click();
     })
 
     it('Ensure User cannot upload incomplete file', () => {
@@ -39,9 +39,9 @@ describe('Billable Hours', () => {
         cy.get('#statement-file').attachFile(fixtureFile);
 
         //Parse New File button
-        cy.get('.btn').click()
+        cy.get('.btn').click();
 
         //Error message displayed
-        cy.get('.alert').should('include.text', 'Column count is less than expected')
+        cy.get('.alert').should('include.text', 'Column count is less than expected');
     })
 })
